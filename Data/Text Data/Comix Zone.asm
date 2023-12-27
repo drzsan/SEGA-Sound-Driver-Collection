b38e2897688a9e766263c85ab3bdace656b2b293
drzsan <darzmistet@gmail.com>
2023-12-27 19:11:35 +0500

    Update Comix Zone.asm
    Comix zone track titles upgrade

--- Data/Text Data/Comix Zone.asm
+++ Data/Text Data/Comix Zone.asm
@@ -30,29 +30,29 @@ Off_PlaylistGEMSCZ_Text: offsetTable
 		offsetTableEntry.w .n25
 ; ---------------------------------------------------------------------------
 
-.n1:		namemstr "Title Screen"
-.n2:		namemstr "Options"
-.n3:		namemstr "Episode 2, Page 1-1"
-.n4:		namemstr "Episode 1, Page 2-2"
-.n5:		namemstr "Episode 2, Page 1-3"
-.n6:		namemstr "Episode 3, Page 1-1"
-.n7:		namemstr "Game Over"
-.n8:		namemstr "Boss Theme"
-.n9:		namemstr "Episode 1, Page 2-1"
-.n10:	namemstr "Episode 1, Page 1-1"
-.n11:		namemstr "Episode 2, Page 1-2"
-.n12:	namemstr "Episode 2, Page 2-1"
-.n13:	namemstr "Episode 1, Page 1-2"
-.n14:	namemstr "Episode 3, Page 2-2"
-.n15:	namemstr "Episode 2, Page 2-2"
-.n16:	namemstr "Episode 3, Page 1-3"
-.n17:	namemstr "Episode 3, Page 1-2"
-.n18:	namemstr "Staff Roll"
-.n19:	namemstr "Introduction"
-.n20:	namemstr "Battle With Mortus"
-.n21:	namemstr "Episode 3, Page 2-3"
-.n22:	namemstr "Ending Theme"
-.n23:	namemstr "Results"
-.n24:	namemstr "Episode 3, Page 2-1"
-.n25:	namemstr "Sega Logo"
+.n1:	namemstr "Sega Logo"								;0				;$18 	Sega Logo	
+.n2:	namemstr "Title Screen"								;1				;0 		Title Screen
+.n3:	namemstr "Into the Zone (Options)"					;2				;1		Into the Zone (Options)
+.n4:	namemstr "Introduction"   							;3				;$12 	Introduction
+.n5:	namemstr "Episode 1, Page 1-1"						;4 				;9 		Episode 1, Page 1-1
+.n6:	namemstr "Seen it for Days (Ep. 1, Page 1-2)"		;5 				;$C 	Seen it for Days (Episode 1, Page 1-2)
+.n7:	namemstr "Feed My Disease (Ep. 1, Page 2-1)"		;6 				;8 		Feed My Disease (Episode 1, Page 2-1)
+.n8:	namemstr "Episode 1, Page 2-2"						;7				;3 		Episode 1, Page 2-2
+.n9:	namemstr "Episode 2, Page 1-1"						;8				;2 		Episode 2, Page 1-1
+.n10:	namemstr "Episode 2, Page 1-2"						;9				;$A 	Episode 2, Page 1-2
+.n11:	namemstr "Last to Follow (Ep. 2, Page 1-3)"			;$A 			;4 		Last to Follow (Episode 2, Page 1-3)
+.n12:	namemstr "Episode 2, Page 2-1"						;$B 			;$B 	Episode 2, Page 2-1
+.n13:	namemstr "10,000 Knives (Ep. 2, Page 2-2)"			;$C 			;$E 	10,000 Knives (Episode 2, Page 2-2)
+.n14:	namemstr "Episode 3, Page 1-1"						;$D 			;5 		Episode 3, Page 1-1
+.n15:	namemstr "Episode 3, Page 1-2"						;$E				;$10 	Episode 3, Page 1-2
+.n16:	namemstr "Episode 3, Page 1-3"						;$F 			;$F 	Episode 3, Page 1-3
+.n17:	namemstr "Episode 3, Page 2-1"						;$10 			;$17 	Episode 3, Page 2-1
+.n18:	namemstr "Episode 3, Page 2-2"						;$11 			;$D 	Episode 3, Page 2-2
+.n19:	namemstr "Episode 3, Page 2-3"						;$12 			;$14 	Episode 3, Page 2-3
+.n20:	namemstr "Woe is the World (Boss Theme)"			;$13 			;7 		Woe is the World (Boss Theme)
+.n21:	namemstr "Battle With Mortus"						;$14 			;$13 	Battle With Mortus
+.n22:	namemstr "Ending Theme"								;$15 			;$15 	Ending Theme
+.n23:	namemstr "Results"									;$16			;$16 	Results
+.n24:	namemstr "Staff Roll"								;$17			;$11 	Staff Roll
+.n25:	namemstr "Game Over"								;$18 			;6 		Game Over
 	even
